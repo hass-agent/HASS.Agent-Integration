a6b91d9c0badabed4ca69e7438429d3be1972c99
AmadeoAlex <68441479+amadeo-alex@users.noreply.github.com>
2025-09-22 16:51:41 +0200

    added notification regarding given device being already configured

--- custom_components/hass_agent/config_flow.py
+++ custom_components/hass_agent/config_flow.py
@@ -83,12 +83,6 @@ class FlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
         self._data = {"device": payload["device"], "apis": payload["apis"]}
 
-        #for config in self._async_current_entries():
-        #    _logger.debug("device: %s, SN: %s, UID: %s", device_name, serial_number, config.unique_id) # TODO(Amadeo): remove
-        #    if config.unique_id == serial_number:
-        #        _logger.debug("device %s, serial number: %s already configured, ignoring", device_name, serial_number)
-        #        return self.async_abort(reason="already_configured")
-
         await self.async_set_unique_id(serial_number)
         self._abort_if_unique_id_configured()
 
@@ -116,25 +110,22 @@ class FlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
             # serial number!
             try:
-
                 def get_device_info():
                     return requests.get(f"{url}/info", timeout=10)
 
                 response = await self.hass.async_add_executor_job(get_device_info)
-
                 response_json = response.json()
-
-                await self.async_set_unique_id(response_json["serial_number"])
-                self._abort_if_unique_id_configured()
-
-                return self.async_create_entry(
-                    title=response_json["device"]["name"],
-                    data={CONF_URL: url},
-                    options={CONF_DEFAULT_NOTIFICATION_TITLE: ATTR_TITLE_DEFAULT},
-                )
-
             except Exception:
                 errors["base"] = "cannot_connect"
+
+            await self.async_set_unique_id(response_json["serial_number"])
+            self._abort_if_unique_id_configured()
+
+            return self.async_create_entry(
+                title=response_json["device"]["name"],
+                data={CONF_URL: url},
+                options={CONF_DEFAULT_NOTIFICATION_TITLE: ATTR_TITLE_DEFAULT},
+            )
 
         return self.async_show_form(
             step_id="local_api",
